0a4f63124e9637a0b0d5e43e89a18bf4985cab1e
Gabriel Fernandes <gabrielwebr@gmail.com>
2019-05-29 19:01:52 -0300

    Organize folders

---
+++ data-structures/tree/bst/Node.java
@@ -0,0 +1,24 @@
+package tree;
+
+public class Node {
+	int data;
+	Node parent;
+	Node left;
+	Node right;
+	
+	Node(int data) {
+		this.data = data;
+	}
+	
+	public void preOrder() {
+		
+	}
+	
+	public void inOrder() {
+		
+	}
+
+	public void postOrder() {
+		
+	}
+}
