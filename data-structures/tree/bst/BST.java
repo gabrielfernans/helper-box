c82df50649b49da113537284b70e7c199f2d7758
Gabriel Fernandes <gabrielwebr@gmail.com>
2019-06-03 21:21:10 -0300

    Update bst

--- data-structures/tree/bst/BST.java
+++ data-structures/tree/bst/BST.java
@@ -1,20 +1,191 @@
-package tree;
+import java.util.Deque;
+import java.util.LinkedList;
 
 public class BST {
-	public Node min(Node node) {
-		return node;
+	private Node root;
+	
+	public void add(Integer element) {
+		if (element != null) {
+			this.add(this.root, element);
+		}
 	}
 	
-	public Node max(Node node) {
-		return node;
+	private void add(Node node, Integer element) {
+		if (isEmpty()) {
+			node.setData(element);
+			node.setLeft(new Node(null));
+			node.setRight(new Node(null));
+			node.getLeft().setParent(node);
+			node.getRight().setParent(node);
+		}
+		
+		else {
+			
+			if (element < node.getData()) {
+				add(node.getLeft(), element);
+			}
+			
+			else if (element > node.getData()) {
+				add(node.getRight(), element);
+			}
+		}
 	}
 	
-	public Node successor(Node node) {
-		return node;
+	public int height() {
+		return height(this.root);
 	}
 	
-	public Node predecessor(Node node) {
-		return node;
+	private int height(Node node) {
+		int result = -1;
+		
+		if (!node.isEmpty()) {
+			int leftHeight = height(node.getLeft());
+			int rightHeight = height(node.getRight());
+			
+			if (leftHeight > rightHeight) {
+				result = leftHeight + 1;
+			}
+			
+			else {
+				result = rightHeight + 1;
+			}
+		}
+		return result;
 	}
 	
+	public Node search(Integer element) {
+		return search(element, this.root);
+	}
+
+	private Node search(Integer element, Node node) {
+		if (node.isEmpty() || element == null) {
+			return new Node(null);
+		}
+		
+		if (element == node.getData()) {
+			return node;
+		}
+		
+		else if (element > node.getData()) {
+			return search(element, node.getLeft());
+		}
+		
+		else {
+			return search(element, node.getRight());
+		}
+	}
+	
+	public Node min() {
+		return min(this.root);
+	}
+	
+	private Node min(Node node) {
+		if (node.getLeft() == null) {
+			return node;
+		}
+		
+		else {
+			return min(node.getLeft());
+		}
+	}
+
+	public Node max() {
+		return max(this.root);
+	}
+	
+	private Node max(Node node) {
+		if (node.getRight() == null) {
+			return node;
+		}
+		
+		else {
+			return max(node.getRight());
+		}
+	}
+
+	public Node successor(Integer element) {
+		Node node = search(element);
+		
+		if (node.getRight() != null) {
+			return min(node.getRight());
+		}
+		
+		Node aux = node;
+		
+		while (aux.getParent() != null) {
+			if (aux.getParent().getData() > node.getData()) {
+				return aux.getParent();
+			}
+			aux = aux.getParent();
+		}
+		return null;
+	}
+
+	public Node predeccessor(Integer element) {
+		Node node = search(element);
+		
+		if (node.getLeft() != null) {
+			return max(node.getLeft());
+		}
+		
+		Node aux = node;
+		
+		while (aux.getParent() != null) {
+			if (aux.getParent().getData() < node.getData()) {
+				return aux.getParent();
+			}
+			aux = aux.getParent();
+		}
+		return null;
+	}
+	
+	public void bfs() {
+		if (!isEmpty()) {
+			Deque<Node> queue = new LinkedList<Node>();
+			
+			queue.addLast(this.root);
+			
+			while(!queue.isEmpty()) {
+				
+				Node node = queue.removeFirst();
+				System.out.println(node.getData());
+				
+				if (node.getLeft() != null) {
+					queue.addLast(node.getLeft());
+				}
+				
+				if(node.getRight() != null) {
+					queue.addLast(node.getRight());
+				}
+			}
+		}
+	}
+	
+	public void preOrder() {
+		
+	}
+	
+	public void inOrder() {
+		
+	}
+	
+	public void postOrder() {
+		
+	}
+	
+	public void remove(Integer element) {
+		
+	}
+	
+	public Node getRoot() {
+		return this.root;
+	}
+	
+	public boolean isEmpty() {
+		return this.root == null;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
 }
--- data-structures/tree/bst/Node.java
+++ data-structures/tree/bst/Node.java
@@ -1,24 +1,55 @@
-package tree;
 
 public class Node {
-	int data;
-	Node parent;
-	Node left;
-	Node right;
 	
-	Node(int data) {
+	private Integer data;
+	private Node parent;
+	private Node left;
+	private Node right;
+	
+	public Node(Integer data) {
 		this.data = data;
 	}
 	
-	public void preOrder() {
-		
-	}
-	
-	public void inOrder() {
-		
+	public boolean isEmpty() {
+		return this.data == null;
 	}
 
-	public void postOrder() {
-		
+	public Integer getData() {
+		return data;
+	}
+
+	public Node getParent() {
+		return parent;
+	}
+
+	public Node getLeft() {
+		return left;
+	}
+
+	public Node getRight() {
+		return right;
+	}
+
+	public void setData(Integer data) {
+		this.data = data;
+	}
+
+	public void setParent(Node parent) {
+		this.parent = parent;
+	}
+
+	public void setLeft(Node left) {
+		this.left = left;
+	}
+
+	public void setRight(Node right) {
+		this.right = right;
+	}
+	
+	public boolean isLeaf() {
+		return this.left == null && this.right == null;
+	}
+	public String toString() {
+		return this.data + "";
 	}
 }
